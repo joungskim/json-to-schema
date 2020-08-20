8c48519d4ae3aa0e9d84b6b5d9876b79747c4094
joungskim <jsk.fni@gmail.com>
2020-08-20 17:27:56 -0500

    Fixed issue where function was being called before initialization.

--- index.js
+++ index.js
@@ -6,19 +6,10 @@ const { fetchData } = require('./dataAccess/getJsonDA');
 //test URLS
 const DND_MONSTER_URL = 'https://dl.dropboxusercontent.com/s/iwz112i0bxp2n4a/5e-SRD-Monsters.json';
 const DND_SPELLS_URL = 'https://dl.dropboxusercontent.com/s/121t7xstyyeofxw/5e-SRD-Spells.json';
+
+//modify the values below.
 const URL = 'https://jsonplaceholder.typicode.com/users';
 const SCHEMA_NAME = "YOUR SCHEMA NAME"
-
-
-//Uncomment/comment which functions you wish to use.
-printMongooseModel(URL, SCHEMA_NAME);
-//testFunction(URL, SCHEMA_NAME)
-
-const testFunction = async() => {
-    console.log(await getMongooseModelArray(URL, SCHEMA_NAME));
-}
-
-
 
 //Prints the mongoose model class in the console.
 const printMongooseModel = async(url, name) => {
@@ -36,6 +27,14 @@ const getMongooseModelArray = async(url, name) => {
     return toMongooseArray(jsonObject, name);
 }
 
+
+//Uncomment/comment which functions you wish to use.
+printMongooseModel(URL, SCHEMA_NAME);
+//testFunction(URL, SCHEMA_NAME)
+
+const testFunction = async() => {
+    console.log(await getMongooseModelArray(URL, SCHEMA_NAME));
+}
 
 module.exports.getMongooseModelArray = getMongooseModelArray;
 module.exports.printMongooseModel = printMongooseModel;
